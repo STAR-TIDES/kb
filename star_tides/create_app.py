bc515a516b93aab90b36e08cdd2c671560e683e2
Evan Kivolowitz <ekivolowitz@wisc.edu>
2021-05-12 18:13:13 -0500

    removing postgres sqlalchemy stuff

--- star_tides/create_app.py
+++ star_tides/create_app.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template
 from mongoengine import connect
-from celery import Celery
 from star_tides.api.blueprint import bp
 from star_tides.api.routes.auth_route import auth
 import os
-
-
 
 def create_app():
     app = Flask(__name__, static_url_path='/static')
     app.config.from_object('star_tides.config.settings')
     app.config['SECRET_KEY'] = os.environ.get('SECRET') or "secret_key"
-
-    db.init_app(app)
 
     connect(
         username='evan',

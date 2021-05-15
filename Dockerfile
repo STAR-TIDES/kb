c46554dff017f0ae1215bb87e11c032d0fb452b1
Evan Kivolowitz <ekivolowitz@wisc.edu>
2021-05-15 01:15:27 -0500

    making changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM python:3.7-stretch
-
 COPY ./star_tides/requirements.txt /tmp/requirements.txt
 RUN pip3 install -r /tmp/requirements.txt
 WORKDIR /app
--- star_tides/api/controllers/test_auth_controller.py
+++ star_tides/api/controllers/test_auth_controller.py
@@ -1,5 +1,7 @@
-''' star_tides.tests.tests.
+''' star_tides.api.routes.test_auth_route
 '''
+# Disabled because pytest adds fixtures by having them specified as arguments.
+# This overloads the import which isn't allowed in google's pylintrc.
 # pragma pylint: disable=W0621,W0613,W0611
 
 from star_tides.tests.fixtures.basic_user import basic_user
@@ -7,6 +9,7 @@ from star_tides.services.mongo.models.user_model import User
 
 
 def test_basic_test(client, basic_user):
+    # This isn't a real test yet.
     basic_user()
 
     u = User.objects(email="test_user@example.com").first()

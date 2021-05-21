651181b725b2e9d4e855103e63f9faa6b78ccfad
Leo Rudberg <ljr@google.com>
2021-05-21 12:15:55 -0400

    Create db/ directory with users.py utility file.

---
+++ star_tides/db/users.py
@@ -0,0 +1,41 @@
+'''star_tides.db.users
+
+This file contains classes and utilities for working with User objects in the
+KB database.
+'''
+
+from typing import List, NamedTuple, Text
+from enum import Enum
+
+
+class UserPrivelege(Enum):
+    UNKNOWN = 0
+    ADMIN = 1
+    COLLABORATOR = 2
+
+class User(NamedTuple):
+    id: Text
+    contact_id: Text
+    username: Text
+    login_email: Text
+    password_hash: Text
+    privilege: UserPrivelege
+
+
+class UsersCollection():
+    '''
+    UsersCollection abstracts CRUD (etc.) operations on the Mongo Users
+    collection.
+    '''
+
+    def get_user(self, unused_id: Text) -> User:
+        raise NotImplementedError()
+
+    def list_users(self) -> List[User]:
+        raise NotImplementedError()
+
+    def create_user(self, user: User) -> User:
+        raise NotImplementedError()
+
+    def delete_user(self, unused_id: Text) -> None:
+        raise NotImplementedError()

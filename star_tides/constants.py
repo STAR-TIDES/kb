e20b1f55dad8ff6f62d9a558494644fce7608e6a
Evan Kivolowitz <ekivolowitz@wisc.edu>
2021-05-25 21:57:41 -0500

    Adding Alphabet comment

--- star_tides/constants.py
+++ star_tides/constants.py
@@ -3,6 +3,9 @@
 This module contains constants to be used throughout the star_tides application.
 
 '''
+
 import string
 
+# All ascii letters (not special chars) and numbers 0-9 for generating
+# random strings
 ALPHABET = string.ascii_letters + string.digits

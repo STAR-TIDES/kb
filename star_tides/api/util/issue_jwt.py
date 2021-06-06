9dc9bf8ddb3644d8eeb168279173565ce22498eb
Evan Kivolowitz <ekivolowitz@wisc.edu>
2021-06-05 20:02:10 -0600

    Added refresh_token

--- star_tides/api/util/issue_jwt.py
+++ star_tides/api/util/issue_jwt.py
@@ -6,15 +6,33 @@ from flask import current_app
 
 
 def create_jwt(email):
-
+    seconds = 60
+    minutes = 60
+    hours = 24
+    days = 7
+    weeks = 4
+    months = 6
     now = int(time.time())
 
-    return jwt.encode({
-        'iss': 'star-tides',
-        'exp': now + 3600,
-        'iat': now,
-        'aud': [f'email:{email}']
+    jwt_token = jwt.encode(
+        {
+            'iss': 'star-tides',
+            'exp': now + seconds * minutes,
+            'iat': now,
+            'claims': {
+                'email': email,
+            }
         },
         current_app.config['SECRET_KEY']
     )
 
+    # Refresh token valid for 6 months
+    refresh_token = jwt.encode({
+        'iss': 'star-tides',
+        'exp': now + seconds * minutes * hours * days * weeks * months,
+        'iat': now,
+        },
+        current_app.config['SECRET_KEY']
+    )
+
+    return jwt_token, refresh_token

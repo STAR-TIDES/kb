40e80f9e94b34c8df2afda77827f6383b44e9261
Evan Kivolowitz <ekivolowitz@wisc.edu>
2021-06-11 00:13:16 -0500

    Disabled (#41)

--- star_tides/api/routes/auth_route.py
+++ star_tides/api/routes/auth_route.py
@@ -6,24 +6,25 @@ Contains routes pertaining to authentication.
 from flask import Blueprint, render_template
 from star_tides.api.routes import build_response
 from star_tides.api.controllers.auth_controller import (
-    LoginController,
-    CreateUserController,
+    # Uncomment when basic auth is added again.
+    # LoginController,
+    # CreateUserController,
     GoogleSignInController
 )
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@auth.route('/login', methods=['POST'])
-def login():
-    response = LoginController().execute()
-    return build_response(response)
-
-
-@auth.route('/new/user', methods=['POST'])
-def create_user():
-    response = CreateUserController().execute()
-    return build_response(response)
+# @auth.route('/login', methods=['POST'])
+# def login():
+#     response = LoginController().execute()
+#     return build_response(response)
+#
+#
+# @auth.route('/new/user', methods=['POST'])
+# def create_user():
+#     response = CreateUserController().execute()
+#     return build_response(response)
 
 
 @auth.route('/gsignin', methods=['GET'])

748adbb5010f6c6c95c8b54d9db5ca3d624723d7
Leo Rudberg <ljr@google.com>
2021-06-14 16:36:21 -0400

    Add test route that does not require auth/login.

--- star_tides/api/blueprint.py
+++ star_tides/api/blueprint.py
@@ -4,6 +4,7 @@ This module contains the main blueprint for star_tides.
 
 '''
 
+from datetime import datetime
 from flask import Blueprint
 from star_tides.api.decorators.login_required import login_required
 bp = Blueprint('bp', __name__)
@@ -13,3 +14,7 @@ bp = Blueprint('bp', __name__)
 @login_required
 def index():
     return "It's an app yo"
+
+@bp.route('/test_route/no_auth')
+def index_no_auth():
+    return "It's an app yo [no auth] %s" % datetime.now()

c23c81b11642ed467d4433885a795c258b2ab142
Leo Rudberg <ljr@google.com>
2021-06-22 10:25:46 -0400

    Add Contact model (document) and associated models, etc. to codebase.  (#42)
    * Add DB models and embedded docs for Contacts.
    
    * Add test route that does not require auth/login.
    
    * Add Availability test to assert present values.
    
    * Address comments.
    
    * Use Python 3 format string.

---
+++ star_tides/services/databases/mongo/models/location_model.py
@@ -0,0 +1,16 @@
+'''
+star_tides.services.databases.mongo.models.location_model
+
+Objects and utilities for working with geographic locations at the database
+level.
+'''
+
+from mongoengine import EmbeddedDocument, IntField, StringField
+
+class LocationModel(EmbeddedDocument):
+    '''Location model for attaching geographic information to a model via a
+    mongoengine.EmbeddedDocumentField.
+    '''
+
+    iso_3166_1_country_code = IntField(required=True)
+    arbitrary_text = StringField(required=False)

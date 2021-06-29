918147e5f0d7f23573ee751d693e45f76b2c879f
Leo Rudberg <ljr@google.com>
2021-06-29 11:35:40 -0400

    Add DeleteContact operation.

--- star_tides/api/routes/contact_route.py
+++ star_tides/api/routes/contact_route.py
@@ -1,22 +1,29 @@
 '''star_tides.api.routes.contact_route
 '''
 
-from star_tides.api.controllers.contact_controller import CreateContactController, GetContactController, ListContactsController
+from star_tides.api.controllers.contact_controller import CreateContactController, DeleteContactController, GetContactController, ListContactsController
 from star_tides.api.routes import build_response
 from flask import Blueprint
 
 contact = Blueprint('contact', __name__, url_prefix='/contacts')
 
+
 @contact.route('/', methods=['GET'])
 def list_contacts():
     return build_response(ListContactsController().execute())
+
 
 @contact.route('/<contact_id>', methods=['GET'])
 def get_contact(contact_id: str):
     return build_response(GetContactController(contact_id).execute())
 
-# FIXME(ljr): Add login_required.
+
 @contact.route('/', methods=['POST'])
 def create_contact():
+    # FIXME(ljr): Add login_required.
     return build_response(CreateContactController().execute())
 
+
+@contact.route('/<contact_id>', methods=['DELETE'])
+def delete_contact(contact_id: str):
+    return build_response(DeleteContactController(contact_id).execute())
